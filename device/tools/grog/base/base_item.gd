a59c848f3e710c1666df06e87a96fe3c7915f7ef
jluini <jluini@gmail.com>
2020-05-30 09:40:24 -0300

    Grog: delete old scripts

--- device/tools/grog/base/base_item.gd
+++
@@ -1,1 +0,0 @@
-extends "res://tools/grog/core/item.gd"
--- device/tools/grog/core/room_resource.gd
+++
@@ -1,8 +0,0 @@
-extends Resource
-
-class_name RoomResource
-
-export (PackedScene) var room_scene
-
-func get_target() -> PackedScene:
-	return room_scene
